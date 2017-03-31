69ba451a578cd6ada31542d57dc41c10341a76e8
Hossam Hammady <github@hammady.net>
2017-03-31 11:20:44 +0300

    Setting AWS region
    Must set environment variable:
    AWS_REGION
    to a valid region as per http://docs.aws.amazon.com/general/latest/gr/rande.html

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,4 @@
 Aws.config[:credentials] = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+Aws.config[:region] = ENV['AWS_REGION']
 S3 = Aws::S3::Resource.new
 S3_BUCKET = S3.bucket(ENV['S3_BUCKET'])
