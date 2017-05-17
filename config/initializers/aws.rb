254877faa6e1968545b1a32a6b65f414b0c61a66
Hossam Hammady <github@hammady.net>
2017-05-17 11:26:35 +0300

    skipping AWS initializer if precompiling assets

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,7 @@
-Aws.config[:credentials] = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-Aws.config[:region] = ENV['AWS_REGION']
-S3 = Aws::S3::Resource.new
-S3_BUCKET = S3.bucket(ENV['S3_BUCKET'])
+# don't connect to AWS if during build process, environment variables may not be ready
+if $*[0] != 'assets:precompile'
+  Aws.config[:credentials] = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+  Aws.config[:region] = ENV['AWS_REGION']
+  S3 = Aws::S3::Resource.new
+  S3_BUCKET = S3.bucket(ENV['S3_BUCKET'])
+end
